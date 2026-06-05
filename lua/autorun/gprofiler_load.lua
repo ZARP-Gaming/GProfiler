b00295dcbe3df40e7e358107bfc02b6e2075b051
callumok2004 <callumokane123@gmail.com>
2026-06-05 03:10:51 +0100

    Change logs formatting

--- lua/autorun/gprofiler_load.lua
+++ lua/autorun/gprofiler_load.lua
@@ -1,20 +1,34 @@
 GProfiler = GProfiler or { Config = {}, Access = {}, Utils = {} }
 
 local logLevels = {
-	[1] = {"DEBUG", Color(0, 255, 0)},
-	[2] = {"INFO", Color(0, 0, 255)},
-	[3] = {"WARNING", Color(255, 255, 0)},
-	[4] = {"ERROR", Color(255, 0, 0)},
-	[5] = {"LOAD", Color(255, 0, 255)}
+	[1] = {"DEBUG", Color(80, 200, 120)},
+	[2] = {"INFO", Color(80, 150, 255)},
+	[3] = {"WARNING", Color(230, 200, 60)},
+	[4] = {"ERROR", Color(235, 75, 75)},
+	[5] = {"LOAD", Color(200, 110, 230)}
 }
 
 local color_white = Color(255, 255, 255)
+local lastLogTime = {}
+
+local function f(ms)
+	if ms >= 86400000 then return math.Round(ms / 86400000) .. "d" end
+	if ms >= 3600000 then return math.Round(ms / 3600000) .. "h" end
+	if ms >= 60000 then return math.Round(ms / 60000) .. "m" end
+	if ms >= 1000 then return math.Round(ms / 1000) .. "s" end
+	return math.Round(ms) .. "ms"
+end
 
 function GProfiler.Log(str, lvl)
 	local LvlData = logLevels[lvl or 1] or logLevels[1]
 	if not GProfiler.Config[string.format("LOG_%s", LvlData[1])] then return end
 
-	MsgC(LvlData[2], string.format("[GProfiler][%s] ", LvlData[1]), color_white, str, "\n")
+	local namespace = "gprofiler:" .. string.lower(LvlData[1])
+	local now = SysTime() * 1000
+	local diff = lastLogTime[namespace] and (now - lastLogTime[namespace]) or 0
+	lastLogTime[namespace] = now
+
+	MsgC(LvlData[2], "  " .. namespace .. " ", color_white, str, LvlData[2], " +" .. f(diff), color_white, "\n")
 end
 
 local incFuncs = {
